7137fee07205a694f3739816577cea9724cd7b52
Naimar1 <naiglyme1@gmail.com>
2019-01-10 12:15:47 +0200

    Add another input

--- scripts.js
+++ scripts.js
@@ -18,3 +18,9 @@ function Contact(first, last) {
       $("input#new-last-name").val("");
     });
   }); 
+    $(".contact").last().click(function() {
+       $("#show-contact").show();
+       $("#show-contact h2").text(newContact.firstName);
+       $(".first-name").text(newContact.firstName);
+       $(".last-name").text(newContact.lastName);
+  });  
